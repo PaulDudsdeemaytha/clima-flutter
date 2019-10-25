e8d3a7f24942c87f1ac0940e291cdb6eb87c57fd
Paul Dudsdeemaytha <pdudsdee@sfu.ca>
2019-10-25 14:16:16 -0700

    added loading screen and navigator

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,10 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import '../services/location.dart';
+import '../services/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const apiKey = '40b158a0b0c200434d00f699e7cb2902';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,6 +12,8 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
   @override
   void initState() {
     super.initState();
@@ -14,26 +21,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    try {
-      Position position = await Geolocator()
-          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } catch (e) {
-      print(e);
-    }
+    Location location = Location();
+    //we can only await if theres a future set up
+    await location.getCurrentLocation();
+    longitude = location.longitude;
+    latitude = location.latitude;
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    var weatherData = await networkHelper.getData();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
-  void 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-//      body: Center(
-//        child: RaisedButton(
-//          onPressed: () {
-//            getLocation();
-//          },
-//          child: Text('Get Location'),
-//        ),
-//      ),
-        );
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 50,
+        ),
+      ),
+    );
   }
 }
